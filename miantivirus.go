3d164be1f3d3847764b3b6168f40ee4cf308407a
mugomes <murilogomes5792@gmail.com>
2026-01-07 19:37:45 -0300

    Remove theme

--- miantivirus.go
+++ miantivirus.go
@@ -6,13 +6,11 @@
 package main
 
 import (
-	"image/color"
 	"net/url"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 
 	"github.com/mugomes/mgcolumnview"
@@ -24,37 +22,6 @@ import (
 )
 
 const VERSION_APP string = "2.0.0"
-
-type myDarkTheme struct{}
-
-func (m myDarkTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
-	// A lógica para forçar o modo escuro é retornar cores escuras.
-	// O Fyne usa estas constantes internamente:
-	switch name {
-	case theme.ColorNameBackground:
-		return color.RGBA{28, 28, 28, 255} // Fundo preto
-	case theme.ColorNameForeground:
-		return color.White // Texto branco
-	// Adicione outros casos conforme a necessidade (InputBackground, Primary, etc.)
-	default:
-		// Retorna o tema escuro padrão para as outras cores (se existirem)
-		// Aqui estamos apenas definindo as cores principais para garantir o Dark Mode
-		return theme.DefaultTheme().Color(name, theme.VariantDark)
-	}
-}
-
-// 3. Implemente os outros métodos necessários da interface fyne.Theme (usando o tema padrão)
-func (m myDarkTheme) Font(s fyne.TextStyle) fyne.Resource {
-	return theme.DefaultTheme().Font(s)
-}
-
-func (m myDarkTheme) Icon(n fyne.ThemeIconName) fyne.Resource {
-	return theme.DefaultTheme().Icon(n)
-}
-
-func (m myDarkTheme) Size(n fyne.ThemeSizeName) float32 {
-	return theme.DefaultTheme().Size(n)
-}
 
 func main() {
 	c.LoadTranslations()
